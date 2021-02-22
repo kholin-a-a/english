f601f6600b17c1018e9183a72d7605a50f46f3c8
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-02-22 11:36:10 +0300

    template for lessons API

---
+++ web-api/English.WebApi/Controllers/LessonsController.cs
@@ -0,0 +1,27 @@
+﻿using English.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace English.WebApi.Controllers
+{
+    public class LessonsController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<LessonOutputModel>> StartLesson()
+        {
+            var model = new LessonOutputModel
+            {
+                Id = 20,
+                Number = 12
+            };
+
+            return Ok(model);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> StopLesson(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ web-api/English.WebApi/Models/Lessons/LessonOutputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace English.WebApi.Models
+{
+    public class LessonOutputModel
+    {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+    }
+}
