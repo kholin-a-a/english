dbfbe9a397c698b0eafccba5dc19e5fff2c1c0c8
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-02-22 11:01:24 +0300

    added StatsController

---
+++ web-api/English.WebApi/Controllers/Base/ApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace English.WebApi.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class ApiControllerBase : ControllerBase
+    {
+    }
+}
---
+++ web-api/English.WebApi/Controllers/StatsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace English.WebApi.Controllers
+{
+    public class StatsController : ApiControllerBase
+    {
+        public async Task<ActionResult> GetStats()
+        {
+            return Ok(
+                new { TotalLessons = 34 }
+                );
+        }
+    }
+}
