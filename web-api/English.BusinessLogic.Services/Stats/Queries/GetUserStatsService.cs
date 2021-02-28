784b9fb76ec9a0b3189a7624e170b6c0cbc8f8a9
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-02-28 14:00:50 +0300

    GetUserStatsService

---
+++ web-api/English.BusinessLogic.Services/Stats/Queries/GetUserStatsService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Services
+{
+    public class GetUserStatsService : IQueryService<GetUserStats, UserStats>
+    {
+        public Task<UserStats> ExecuteAsync(GetUserStats query)
+        {
+            return Task.FromResult(
+                new UserStats
+                {
+                    TotalLessons = 123
+                });
+        }
+    }
+}
