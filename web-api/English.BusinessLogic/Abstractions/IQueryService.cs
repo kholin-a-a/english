0f656d0f115c3d426756cf882c29fea7fbb2c19c
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-02-28 12:28:40 +0300

    IQueryService

---
+++ web-api/English.BusinessLogic/Abstractions/IQueryService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace English.BusinessLogic
+{
+    public interface IQueryService<TQuery, TResult>
+    {
+        Task<TResult> ExecuteAsync(TQuery query);
+    }
+}
