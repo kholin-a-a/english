f4b0aec6eaafc87fb7f7fc35a3560641709955bf
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-02-28 13:17:51 +0300

    stubs for lesson services

---
+++ web-api/English.BusinessLogic.Services/Lessons/Commands/StartLessonService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Services
+{
+    public class StartLessonService : ICommandService<StartLesson>
+    {
+        public Task ExecuteAsync(StartLesson command)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ web-api/English.BusinessLogic.Services/Lessons/Queries/GetCurrentLessonService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Services
+{
+    public class GetCurrentLessonService : IQueryService<GetCurrentLesson, Lesson>
+    {
+        public Task<Lesson> ExecuteAsync(GetCurrentLesson query)
+        {
+            return Task.FromResult(
+                new Lesson
+                {
+                    Id = 12,
+                    Number = 34
+                });
+        }
+    }
+}
