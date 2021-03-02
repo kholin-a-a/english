27cc255b5658eb8301deba239496398ec6ac6c69
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-02 12:32:25 +0300

    CompletedWord model

---
+++ web-api/English.BusinessLogic/Words/Models/CompletedWord.cs
@@ -0,0 +1,11 @@
+﻿namespace English.BusinessLogic
+{
+    public class CompletedWord
+    {
+        public int WordId { get; set; }
+
+        public int LessonId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
