139ae8f6e5a63be718bce6c73505ab39c0710dca
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-03 13:03:11 +0300

    cleaned GetUserStatsServiceTests

--- web-api/English.BusinessLogic.Services.Tests/Stats/Queries/GetUserStatsServiceTests.cs
+++ web-api/English.BusinessLogic.Services.Tests/Stats/Queries/GetUserStatsServiceTests.cs
@@ -1,5 +1,4 @@
 ﻿using Moq;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
@@ -78,41 +77,6 @@ namespace English.BusinessLogic.Services.Tests
             // Assert
             Assert.Equal(user.Lessons.Count, stats.TotalLessons);
         }
-
-        //[Fact]
-        //public async Task ExecuteAsync_NullQuery_ThrowsException()
-        //{
-        //    var service = this.MakeService();
-
-        //    await Assert.ThrowsAsync<ArgumentNullException>(() => service.ExecuteAsync(null));
-        //}
-
-        //[Fact]
-        //public async Task ExecuteAsync_Default_TotalLessonsEqualLessonsCount()
-        //{
-        //    // Setup
-        //    var service = this.MakeService();
-        //    var count = 77;
-        //    var userId = 120;
-
-        //    this._lessonRepoMock.Setup(m =>
-        //            m.GetLessonCount(It.Is<int>(id => id == userId))
-        //        )
-        //        .ReturnsAsync(count);
-
-        //    this._userContextMock.Setup(m =>
-        //            m.UserId
-        //        )
-        //        .Returns(userId);
-
-        //    // Action
-        //    var fact = await service.ExecuteAsync(
-        //            new GetUserStatsQuery()
-        //        );
-
-        //    // Assert
-        //    Assert.Equal(count, fact.TotalLessons);
-        //}
 
         private GetUserStatsService MakeService(
             IUserContext context = null,
