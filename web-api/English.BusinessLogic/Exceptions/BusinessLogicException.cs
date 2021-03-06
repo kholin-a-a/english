42133d9b9621c83ae0df3e0d38161cb078eaf220
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-06 15:16:53 +0300

    added BusinesLogicExceptions

---
+++ web-api/English.BusinessLogic/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace English.BusinessLogic
+{
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException(): base()
+        { }
+
+        public BusinessLogicException(string message): base(message)
+        { }
+    }
+}
---
+++ web-api/English.BusinessLogic/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace English.BusinessLogic
+{
+    public class EntityNotFoundException : BusinessLogicException
+    {
+        public EntityNotFoundException(string message) : base(message)
+        { }
+    }
+}
