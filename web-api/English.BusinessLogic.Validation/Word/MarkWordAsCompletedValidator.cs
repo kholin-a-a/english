85f88d4748fea5b677af2a12c5fe3cbcfa908e84
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-06 15:20:37 +0300

    added MarkWordAsCompletedValidator

---
+++ web-api/English.BusinessLogic.Validation/Word/MarkWordAsCompletedValidator.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Validation
+{
+    public class MarkWordAsCompletedValidator : ICommandValidator<MarkWordAsCompletedCommand>
+    {
+        private readonly ICommandService<MarkWordAsCompletedCommand> _decoratee;
+        private readonly IUserRepository _userRepo;
+        private readonly IWordRepository _wordRepo;
+        private readonly IUserContext _userContext;
+
+        public MarkWordAsCompletedValidator(
+            ICommandService<MarkWordAsCompletedCommand> decoratee,
+            IUserRepository userRepo,
+            IWordRepository wordRepo,
+            IUserContext userContext
+        )
+        {
+            this._decoratee = decoratee;
+            this._userRepo = userRepo;
+            this._wordRepo = wordRepo;
+            this._userContext = userContext;
+        }
+
+        public async Task Validate(MarkWordAsCompletedCommand command)
+        {
+            var word = await this._wordRepo.Find(command.WordId);
+            if (word == null)
+                throw new EntityNotFoundException($"Word with id {command.WordId} is not found");
+
+            var user = await this._userRepo.Find(this._userContext.UserId);
+
+            var lessonExists = user.Lessons.Any(l => l.Id == command.LessonId);
+            if (!lessonExists)
+                throw new EntityNotFoundException($"Lesson with id {command.LessonId} is not found");
+
+            await this._decoratee.ExecuteAsync(command);
+        }
+    }
+}
