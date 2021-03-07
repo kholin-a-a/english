ecc0439bf148a9afe22b6cd7d28a7155c329a858
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-07 11:28:16 +0300

    added MarkWordAsUknownValidator

---
+++ web-api/English.BusinessLogic.Validation/Word/MarkWordAsUknownValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Validation
+{
+    public class MarkWordAsUknownValidator : ICommandValidator<MarkWordAsUknownCommand>
+    {
+        public Task Validate(MarkWordAsUknownCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
