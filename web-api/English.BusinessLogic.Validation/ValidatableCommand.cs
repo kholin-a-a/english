9b53ddad944ac1b59cb209c2e2a91d7d497e0f12
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-07 12:57:33 +0300

    added ValidatableCommand

---
+++ web-api/English.BusinessLogic.Validation/ValidatableCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Validation
+{
+    public class ValidatableCommand<TCommand> : ICommandService<TCommand>
+    {
+        private readonly ICommandService<TCommand> _commandService;
+        private readonly ICommandValidator<TCommand> _commandValidator;
+
+        public ValidatableCommand(
+            ICommandService<TCommand> commandService,
+            ICommandValidator<TCommand> commandValidator
+        )
+        {
+            this._commandService = commandService;
+            this._commandValidator = commandValidator;
+        }
+
+        public async Task ExecuteAsync(TCommand command)
+        {
+            await this._commandValidator.Validate(command);
+            await this._commandService.ExecuteAsync(command);
+        }
+    }
+}
