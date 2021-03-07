413477356ab17ace1e53e9bf5da390c205866241
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-07 12:27:00 +0300

    WordRules

---
+++ web-api/English.BusinessLogic.Validation.Tests/Rules/WordRuleTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace English.BusinessLogic.Validation.Tests
+{
+    public class WordRuleTests
+    {
+        private readonly WordRepoFake _wordRepoFake;
+
+        public WordRuleTests()
+        {
+            this._wordRepoFake = new WordRepoFake();
+        }
+
+        [Fact]
+        public async Task WordExists_Default_NoExceptions()
+        {
+            var rules = this.MakeRules();
+            await rules.WordExists(1);
+        }
+
+        [Fact]
+        public async Task WordExists_WordIsNull_NotFoundException()
+        {
+            this._wordRepoFake.Word = null;
+            
+            var rules = this.MakeRules();
+
+            var ex = await Assert.ThrowsAsync<EntityNotFoundException>(async () =>
+            {
+                await rules.WordExists(1);
+            });
+
+            Assert.Contains("Word is not found", ex.Message);
+        }
+
+        private WordRules MakeRules(
+            IWordRepository wordRepo = null
+        )
+        {
+            return new WordRules(
+                wordRepo ?? this._wordRepoFake
+                );
+        }
+    }
+}
---
+++ web-api/English.BusinessLogic.Validation/Rules/WordRules.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Validation
+{
+    public class WordRules : IWordRules
+    {
+        private readonly IWordRepository _wordRepo;
+
+        public WordRules(IWordRepository wordRepo)
+        {
+            this._wordRepo = wordRepo;
+        }
+
+        public async Task WordExists(int wordId)
+        {
+            var word = await this._wordRepo.Find(wordId);
+            if (word == null)
+                throw new EntityNotFoundException($"Word is not found");
+        }
+    }
+}
