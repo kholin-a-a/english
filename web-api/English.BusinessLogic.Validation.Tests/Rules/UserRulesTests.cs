5dac8fa04d4c30659c941d1eb15d04fac0ef3f80
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-07 12:37:40 +0300

    UserRules

---
+++ web-api/English.BusinessLogic.Validation.Tests/Rules/UserRulesTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace English.BusinessLogic.Validation.Tests
+{
+    public class UserRulesTests
+    {
+        private readonly UserRepoFake _userRepoFake;
+
+        public UserRulesTests()
+        {
+            this._userRepoFake = new UserRepoFake();
+        }
+
+        [Fact]
+        public async Task HasLesson_HasLesson_NoExceptions()
+        {
+            var lessonId = 2;
+
+            this._userRepoFake.User.Lessons.Add(
+                new Lesson { Id = lessonId }
+                );
+
+            var rules = this.MakeRules();
+
+            await rules.HasLesson(1, lessonId);
+        }
+
+        [Fact]
+        public async Task HasLesson_NoLesson_NotFoundException()
+        {
+            this._userRepoFake.User.Lessons.Clear();
+            var rules = this.MakeRules();
+
+            var ex = await Assert.ThrowsAsync<EntityNotFoundException>(async () =>
+            {
+                await rules.HasLesson(1, 2);
+            });
+
+            Assert.Contains("Lesson is not found", ex.Message);
+        }
+
+        private UserRules MakeRules(
+            IUserRepository userRepo = null
+        )
+        {
+            return new UserRules(
+                userRepo ?? this._userRepoFake
+                );
+        }
+    }
+}
---
+++ web-api/English.BusinessLogic.Validation/Rules/UserRules.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace English.BusinessLogic.Validation
+{
+    public class UserRules : IUserRules
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UserRules(
+            IUserRepository userRepo
+        )
+        {
+            this._userRepo = userRepo;
+        }
+
+        public async Task HasLesson(int userId, int lessonId)
+        {
+            var user = await this._userRepo.Find(userId);
+
+            var lessonExists = user.Lessons.Any(l => l.Id == lessonId);
+            if (!lessonExists)
+                throw new EntityNotFoundException($"Lesson is not found");
+        }
+    }
+}
